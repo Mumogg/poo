b954bb56d00417ce75f371fb8f2087ec03ed255d
Sebastian Murillo <104529023+Mumogg@users.noreply.github.com>
2024-03-13 12:05:43 -0600

    final seccion 14

--- src/EjemploAutomovil.java
+++ src/EjemploAutomovil.java
@@ -1,25 +1,45 @@
+import java.util.Date;
+
 public class EjemploAutomovil {
     public static void main(String[] args) {
 
-        Automovil subaru = new Automovil("Subaru", "Impreza", "Blanco", 2.0);
-        
+        Motor motorSubaru = new Motor(2.0, TipoMotor.BENCINA);
+        Automovil subaru = new Automovil("Subaru", "Impreza");
+        subaru.setMotor(motorSubaru);
+        subaru.setEstanque(new Estanque());
+        subaru.setColor(Color.BLANCO);
 
-        Automovil mazda = new Automovil(
-        "Mazda", 
-        "BT-50", 
-        "Blanco", 
-        3.0);
-    
-        Automovil nissan = new Automovil("Nissan", "Navara", "Gris oscuro");
+        Motor motorMazda = new Motor(3.0, TipoMotor.DIESEL);
+        Automovil mazda = new Automovil("Mazda", "BT-50", Color.ROJO, motorMazda);
+        mazda.setEstanque(new Estanque(45));
+        System.out.println("mazda.fabricante = " + mazda.getFabricante());
 
-        System.out.println(mazda.verDetalle());
+        Automovil nissan = new Automovil("Nissan", "Navara", Color.GRIS,
+                new Motor(4.0, TipoMotor.DIESEL), new Estanque(50));
+
+        Automovil nissan2 = new Automovil("Nissan", "Navara", Color.GRIS,
+                new Motor(3.5, TipoMotor.BENCINA), new Estanque(50));
+
+        Automovil auto = new Automovil();
+        Date fecha = new Date();
+        System.out.println("son iguales? " + (nissan == nissan2));
+        System.out.println("son iguales con equals? " + (nissan.equals(nissan2)));
         System.out.println(subaru.verDetalle());
+        System.out.println(mazda.verDetalle());
         System.out.println(nissan.verDetalle());
+
+        System.out.println(auto.equals(fecha));
+        System.out.println(nissan);
+        System.out.println(nissan.toString());
+
         System.out.println(subaru.acelerar(3000));
         System.out.println(subaru.frenar());
-        System.out.println(mazda.acelerarFrenar(3000));
 
-        System.out.println("kilometros por litro " + subaru.calcularConsumo(300, 0.75f));
-        System.out.println();
+        System.out.println(mazda.acelerarFrenar(4000));
+
+        System.out.println("Kilómetros por litro: " + subaru.calcularConsumo(300, 0.6f));
+
+        System.out.println("Kilómetros por litro: " + subaru.calcularConsumo(300, 60));
+        System.out.println("Kilómetros por litro: " + nissan.calcularConsumo(300, 60));
     }
 }
